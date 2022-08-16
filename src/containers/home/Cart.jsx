6431c0b82b1fb14a86090ce67d37081b51a0b48c
photenix <juanmanuelpinor@live.com>
2022-08-16 16:44:49 -0500

    change

--- src/containers/home/Cart.jsx
+++ src/containers/home/Cart.jsx
@@ -4,6 +4,10 @@ import CardProductAD from '../../modules/CardProductAD';
 import '../../styles/sass/GeneritView.scss'
 
 const Cart = () => {
+
+    const handleOnClick = e => {
+        console.log('Esto se va a descontrolar')
+    }
     return (
         <div className='generit-f-c'>
             <h2 className='text-main'>Favorite</h2>
@@ -11,7 +15,7 @@ const Cart = () => {
                 <CardProductAD name={'Come comida 3000'} price={ Math.PI } distance={35}/>
                 <CardProductAD name={'Come comida 3000'} price={ 5000 } distance={35}/>
             </div>
-            <button className="buyProducts">Comprar</button>
+            <button className="buyProducts" onClick={ handleOnClick }>Comprar</button>
         </div>
     );
 };

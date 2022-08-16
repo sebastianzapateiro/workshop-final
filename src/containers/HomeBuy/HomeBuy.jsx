7f92fd20b42828838b0060415f1da8880c429c70
EstebanGilabert <gilabertesteban2022@gmail.com>
2022-08-16 18:13:46 -0500

    sub

--- src/containers/HomeBuy/HomeBuy.jsx
+++ src/containers/HomeBuy/HomeBuy.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import DataUser from '../../modules/home/DataUser';
-import Recently from '../../modules/home/Recently';
+import NavB from '../../modules/home/NavB';
 import Tips from '../../modules/home/Tips';
 
+import Recently from '../../modules/home/Recently';
 
 
 
@@ -12,7 +13,7 @@ const HomeBuy = () => {
         <DataUser/>
          <Tips/>
          <Recently/>
-
+         <NavB/>
         </div>
     );
 };

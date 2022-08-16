f6301dde4de924ed62486152dfeea77fa0dfef08
photenix <juanmanuelpinor@live.com>
2022-08-16 15:49:47 -0500

    change

--- src/modules/CardProduct.jsx
+++ src/modules/CardProduct.jsx
@@ -4,7 +4,7 @@ const CardProduct = ({ name, price, distance }) => {
     return (
         <div className='card-product'>
             <img src="" alt="not found"  className='card-product-img'/>
-            <footer className='card-product-footer'>
+            <footer className='card-product-text'>
                 <h2>{`${price} $`}</h2>
                 <div>
                     <h3 className='grey'>{ name }</h3>
